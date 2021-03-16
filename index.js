a6ef75ed61fd6b4569ade439d7d257a00ed0e008
komiyak <kosuke.komiya@gmail.com>
2021-03-16 23:24:27 +0900

    Design the '/search' api with openapi.

--- index.js
+++ index.js
@@ -20,6 +20,52 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+/**
+ * @openapi
+ * /search:
+ *  get:
+ *    description: Full-text search of products.
+ *    parameters:
+ *      - in: query
+ *        name: q
+ *        schema:
+ *          type: string
+ *          minLength: 1
+ *          maxLength: 64
+ *        required: true
+ *    responses:
+ *      200:
+ *        description: Returns searching results.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                data:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: string
+ *                        example: UNIQUEID
+ *                      name:
+ *                        type: string
+ *                        description: A product's name
+ *                        example: 笹だんご
+ *                      price:
+ *                        type: integer
+ *                        description: A product's price
+ *                        example: 120
+ *                      thumbnailUrl:
+ *                        type: string
+ *                        description: A product's thumbnail url
+ *                        example: https://example.com/image/xxx.png
+ */
+app.get('/search', (req, res) => {
+  res.send('GET /search')
+})
+
 app.listen(port, () => {
   console.log(`Listening at localhost:${port}`)
 })
